5ddff56194935742cbf6f71e8a005ed8f3197de6
jwatmore <jason.watmore@hotmail.com>
2019-01-03 21:33:34 -0800

    Added auto-logout on 403 and updated to Angular 7.1.4

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -11,8 +11,8 @@ export class ErrorInterceptor implements HttpInterceptor {
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(catchError(err => {
-            if (err.status === 401) {
-                // auto logout if 401 response returned from api
+            if ([401, 403].indexOf(err.status) !== -1) {
+                // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
                 this.authenticationService.logout();
                 location.reload(true);
             }
